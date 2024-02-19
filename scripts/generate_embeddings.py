dc59d7da601f494d73872325efde849bb8c4f999
Kishorè Shanto <tinykishore@icloud.com>
2024-02-19 16:33:03 +0600

    Update

--- scripts/generate_embeddings.py
+++ scripts/generate_embeddings.py
@@ -5,15 +5,13 @@ USAGE: This generate_embedding.py is used to generate embeddings for a given fol
 
 from CreateEmbeddings import Dataset, Window
 
-x = Window.Window('../dataset/anomaly/10_Arson002_x264_010.mp4',
-                  class_label_index=2, true_class_name='anomaly')
-
-print(x.class_label)
-
 embeddings, labels = Dataset.create_dataset(
     '../dataset/',
     class_label_index=2,
     true_class_name='anomaly'
 )
+
+print("Embeddings Shape: ", embeddings.shape)
+print("Labels Shape: ", labels.shape)
 
 # This need to be checked
