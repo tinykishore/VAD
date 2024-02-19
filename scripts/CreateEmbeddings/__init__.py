0db0e37733565ffc741355d178500a118ae2efd4
Kishorè Shanto <tinykishore@icloud.com>
2024-02-19 18:21:49 +0600

    Documented __init__.py

--- scripts/CreateEmbeddings/__init__.py
+++ scripts/CreateEmbeddings/__init__.py
@@ -1,13 +1,26 @@
-import torch
+"""
+Library for generating embedding of videos for a given folder.
+"""
 
+# For importing * from this library
 __all__ = ['Window', 'Dataset', 'EmbeddingModel']
 
-device = torch.device(
-    "mps"
-    if torch.backends.mps.is_available()
-    else
-    ("cuda"
-     if torch.cuda.is_available()
-     else
-     "cpu")
-)
+import torch
+import os
+
+# Set the device to use (e.g., 'cpu', 'cuda', 'mps')
+device = torch.device("mps" if torch.backends.mps.is_available() else
+                      ("cuda" if torch.cuda.is_available() else "cpu")
+                      )
+
+# Select Device According to Availability
+print("Device selected:", device)
+
+# If the device is CUDA, print the device capability
+if device.type == "cuda":
+    os.system("nvidia-smi")
+    print()
+    print("Device type:", device.type)
+    print("Capability:", torch.cuda.get_device_capability(device))
+else:
+    print("Device capabilities are limited on MPSs and CPUs.")
