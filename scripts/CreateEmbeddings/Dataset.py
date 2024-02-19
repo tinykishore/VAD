06ef6ec70b86ff6bdaefa7fe9a9b16cf44ba91f7
Kishorè Shanto <tinykishore@icloud.com>
2024-02-19 18:21:32 +0600

    Documented Dataset.py

--- scripts/CreateEmbeddings/Dataset.py
+++ scripts/CreateEmbeddings/Dataset.py
@@ -1,15 +1,61 @@
+"""
+This file contains the function to create the dataset from the videos. Dataset means the embeddings and labels. The
+embeddings are the features extracted from the videos and the labels are the class labels for the videos. The class
+labels are 0 for non-anomaly and 1 for anomaly.
+"""
+
+# Importing the required libraries
 from random import shuffle
 from glob import glob
 import numpy as np
 from tqdm import tqdm
-
-__all__ = ['create_dataset']
-
 from .Window import Window, WindowEmbedded
 
 
-def create_dataset(file_path: str, class_label_index: int,
-                   true_class_name: str = 'anomaly', shuffle_data: bool = True, video_ext: str = 'mp4'):
+# Function to create the dataset
+def create_dataset(file_path: str,
+                   class_label_index: int,
+                   true_class_name: str = 'anomaly',
+                   shuffle_data: bool = True,
+                   video_ext: str = 'mp4',
+                   save: bool = True):
+    """
+    This function will take the folder path and class_label_index as input and will return the embeddings and
+    labels. We can also specify the true_class_name, shuffle_data and video_ext.
+
+    Parameters
+    ----------
+    file_path : str
+        The folder path containing the videos.
+    class_label_index : int
+        The index of the class name in the file path. For example, if the file path is
+        'dataset/anomaly/1.mp4', then the class_label_index will be 1. The index starts from 0.
+    true_class_name : str, optional
+        The true class name, where the class label is 1.
+        Default: 'anomaly'
+    shuffle_data : bool, optional
+        Whether to shuffle the data or not.
+        Default: True
+    video_ext : str, optional
+        The extension of the videos.
+        Default: 'mp4'
+    save : bool, optional
+        Whether to save the embeddings and labels or not.
+
+    Notes
+    ----------
+    The embeddings will be of shape [x, 4, 24, 1024] and labels will be of shape [x]. The labels will be 0 for non-anomaly
+    and 1 for anomaly. The embeddings and labels will be saved in the same folder with the name embeddings.npy and
+    labels.npy. Some videos may have less than 4 frames or 24 features. This problem can be solved by padding the videos
+    with zeros to make them of the same shape. Will be done in the future.
+
+    Returns
+    ----------
+    all_embeddings : np.ndarray
+        The embeddings and labels of the videos.
+    all_labels : np.ndarray
+        The labels of the videos.
+    """
 
     # Create glob path for the videos
     rgx = file_path + f'/*/*.{video_ext}'
@@ -37,9 +83,10 @@ def create_dataset(file_path: str, class_label_index: int,
             print(f"\n\033[91mError windowing video: {video_path}\033[0m")
             continue
 
-    # Save the embeddings and labels
+    # Save the embeddings and labels and return
     all_embeddings = np.array(all_embeddings)
     all_labels = np.array(all_labels)
-    np.save('embeddings.npy', all_embeddings)
-    np.save('labels.npy', all_labels)
+    if save:
+        np.save('embeddings.npy', all_embeddings)
+        np.save('labels.npy', all_labels)
     return all_embeddings, all_labels
